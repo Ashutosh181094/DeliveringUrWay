a927319a3dbdf148291a3e6d9e7f645db47b5176
kshitijsharma97 <kshtjsharma9@gmail.com>
2018-02-11 14:55:53 +0530

    slider dekh lena

--- app/src/main/java/com/example/a1505197/deliveringurway/MainActivity.java
+++ app/src/main/java/com/example/a1505197/deliveringurway/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.a1505197.deliveringurway;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+
     }
 }
