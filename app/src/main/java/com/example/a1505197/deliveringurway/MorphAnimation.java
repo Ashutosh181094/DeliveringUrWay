13f26d51bb7d439934d411d78086ee8c9a6317c5
Ashutosh181094 <ashutoshrai181094@gmail.com>
2018-05-28 03:52:24 +0530

    Forgot Password

---
+++ app/src/main/java/com/example/a1505197/deliveringurway/MorphAnimation.java
@@ -0,0 +1,70 @@
+package com.example.a1505197.deliveringurway;
+
+import android.animation.LayoutTransition;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+/**
+ * Created by 1505197 on 5/28/2018.
+ */
+
+public class MorphAnimation {
+
+    private final FrameLayout parentView;
+    private View buttonContainer;
+    private ViewGroup viewsContainer;
+
+    private boolean isPressed;
+    private int initialWidth;
+    private int initialGravity;
+
+    public boolean isPressed() {
+        return isPressed;
+    }
+
+    public MorphAnimation(View buttonContainer, FrameLayout parentView, ViewGroup viewsContainer) {
+        this.buttonContainer = buttonContainer;
+        this.parentView = parentView;
+        this.viewsContainer = viewsContainer;
+
+        LayoutTransition layoutTransition = parentView.getLayoutTransition();
+        layoutTransition.setDuration(400);
+        layoutTransition.enableTransitionType(LayoutTransition.CHANGING);
+
+        isPressed = false;
+
+    }
+
+    public void morphIntoForm() {
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) buttonContainer.getLayoutParams();
+
+        initialWidth = layoutParams.width;
+        initialGravity = layoutParams.gravity;
+
+        layoutParams.gravity = Gravity.CENTER;
+        layoutParams.width = FrameLayout.LayoutParams.MATCH_PARENT;
+        buttonContainer.setLayoutParams(layoutParams);
+
+        for (int i = 1; i < viewsContainer.getChildCount(); i++) {
+            viewsContainer.getChildAt(i).setVisibility(View.VISIBLE);
+        }
+
+        isPressed = true;
+    }
+
+    public void morphIntoButton() {
+        for (int i = 1; i < viewsContainer.getChildCount(); i++) {
+            viewsContainer.getChildAt(i).setVisibility(View.GONE);
+        }
+
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) buttonContainer.getLayoutParams();
+        layoutParams.gravity = initialGravity;
+        layoutParams.width = initialWidth;
+        buttonContainer.setLayoutParams(layoutParams);
+
+        isPressed = false;
+    }
+
+}
