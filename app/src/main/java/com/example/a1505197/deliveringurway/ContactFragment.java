63477f7a566d9ff353a9c4718ae16d4c1721afd7
Ashutosh181094 <ashutoshrai181094@gmail.com>
2018-05-28 13:28:13 +0530

    Forgot Password

--- app/src/main/java/com/example/a1505197/deliveringurway/ContactFragment.java
+++ app/src/main/java/com/example/a1505197/deliveringurway/ContactFragment.java
@@ -1,9 +1,21 @@
 package com.example.a1505197.deliveringurway;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class ContactFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_contact,container,false);
+        return view;
 
+    }
 }
 //
