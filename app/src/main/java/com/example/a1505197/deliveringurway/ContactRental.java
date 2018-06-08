8b74634b20893b03e473fe1a2ed225b3082cbcf4
Ashutosh181094 <ashutoshrai181094@gmail.com>
2018-06-08 15:02:18 +0530

    Contact Info

---
+++ app/src/main/java/com/example/a1505197/deliveringurway/ContactRental.java
@@ -0,0 +1,21 @@
+package com.example.a1505197.deliveringurway;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ContactRental extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_contact_rental,container,false);
+        return view;
+
+    }
+}
+//
