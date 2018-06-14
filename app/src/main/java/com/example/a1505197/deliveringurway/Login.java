355ec85ca64c4a306d2796ad67ff147f58963c84
Vivek <vivekkumarsharma9746@gmail.com>
2018-06-15 00:46:46 +0530

    Alert Dialog

--- app/src/main/java/com/example/a1505197/deliveringurway/Login.java
+++ app/src/main/java/com/example/a1505197/deliveringurway/Login.java
@@ -1,5 +1,7 @@
 package com.example.a1505197.deliveringurway;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -219,6 +221,29 @@ public class Login extends AppCompatActivity
           // signed in
         }
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
 }
