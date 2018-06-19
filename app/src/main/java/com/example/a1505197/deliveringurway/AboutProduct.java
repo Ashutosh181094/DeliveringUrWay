afd88d69ecdd2c3c403d54ed354f6f71f64dd7a9
Ashutosh181094 <ashutoshrai181094@gmail.com>
2018-06-20 04:20:25 +0530

    NewChanges

--- app/src/main/java/com/example/a1505197/deliveringurway/AboutProduct.java
+++ app/src/main/java/com/example/a1505197/deliveringurway/AboutProduct.java
@@ -13,6 +13,11 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +31,7 @@ import com.stepstone.apprating.listener.RatingDialogListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class AboutProduct extends AppCompatActivity implements RatingDialogListener
 {
@@ -40,8 +46,10 @@ CommentAdapter commentAdapter;
 ArrayList<Rating> ratings;
 RecyclerView recyclerView;
 String Comment;
-TextView Description;
+TextView Description,numofComment;
 int Rating,Ratingsum=0;
+int a[]=new int[5];
+int ratingone=0,ratingtwo=0,ratingthree=0,ratingfour=0,ratingfive=0;
     ArrayList<FoodProductDescription> userSideFoodProductDescription;
     ArrayList<ClothProductDescription> clothProductDescriptions;
     ArrayList<RentalProductDescription> rentalProductDescriptions;
@@ -58,6 +66,7 @@ int Rating,Ratingsum=0;
         Description=findViewById(R.id.product_description);
         imageView=findViewById(R.id.itemImage);
         ratings=new ArrayList<>();
+        numofComment=findViewById(R.id.numofComment);
         productName=intent.getStringExtra("productName");
         phoneNumber=intent.getStringExtra("phoneNumber");
         Toast.makeText(getApplicationContext(),""+productName+""+phoneNumber,Toast.LENGTH_SHORT).show();
@@ -66,7 +75,12 @@ int Rating,Ratingsum=0;
         userSideFoodProductDescription =new ArrayList<>();
         clothProductDescriptions=new ArrayList<>();
         rentalProductDescriptions=new ArrayList<>();
-
+       final String s[]=new String[5];
+        s[0]="ONE";
+        s[1]="TWO";
+        s[2]="THREE";
+        s[3]="FOUR";
+        s[4]="FIVE";
 
         databaseReferenceRatings=FirebaseDatabase.getInstance().getReference().child("+91"+phoneNumber).child(productName);
         databaseReferenceRatings.addValueEventListener(new ValueEventListener() {
@@ -75,14 +89,62 @@ int Rating,Ratingsum=0;
                 if (dataSnapshot.exists())
                 {
                  ratings.clear();
+                 Ratingsum=0;
+                 ratingone=0;
+                 ratingtwo=0;
+                 ratingthree=0;
+                 ratingfour=0;
+                 ratingfive=0;
                     for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren())
                     {
                         Rating rating = dataSnapshot1.getValue(Rating.class);
                         ratings.add(rating);
+
+                        if(rating.rating==1)
+                        {
+                            ratingone++;
+                        }
+                        else
+                            if(rating.rating==2)
+                            {
+                                ratingtwo++;
+                            }
+                            else
+                            if(rating.rating==3)
+                            {
+                                ratingthree++;
+                            }
+                            else
+                            if(rating.rating==4)
+                            {
+                                ratingfour++;
+                            }
+                            else
+                            {
+                               ratingfive++;
+                            }
                         Ratingsum=Ratingsum+rating.rating;
                     }
+                    a[0]=ratingone;
+                    a[1]=ratingtwo;
+                    a[2]=ratingthree;
+                    a[3]=ratingfour;
+                    a[4]=ratingfive;
+                    List<PieEntry> p=new ArrayList<>();
+                    for(int i=0;i<5;i++)
+                    {
+                        p.add(new PieEntry(a[i],s[i]));
+                    }
+                    PieDataSet pieDataSet=new PieDataSet(p,"Rating");
+                    pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+                    PieData pieData=new PieData(pieDataSet);
+                    PieChart chart=findViewById(R.id.ratingChart);
+                    chart.setData(pieData);
+                    chart.animateY(1000);
+                    chart.invalidate();
 
-                    Toast.makeText(getApplicationContext(),""+ratings.size(),Toast.LENGTH_LONG).show();
+                    numofComment.setText("("+ratings.size()+")");
+
                     CommentAdapter commentsAdapter=new CommentAdapter(AboutProduct.this,ratings);
                     recyclerView.setAdapter(commentsAdapter);
                     recyclerView.setHasFixedSize(true);
